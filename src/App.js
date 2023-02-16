7f399a5d5d7566cc9b4499e7f77d201197622a3e
Mudasir Issah <danladi496@gmail.com>
2023-02-16 04:08:16 +0000

    fixed-linters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Calculator from './components/calculator'
+import Calculator from './components/calculator';
 
 function App() {
   return (
