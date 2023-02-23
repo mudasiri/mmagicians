bd63c3de3175c14cae49dd3884c50f5231efc719
Mudasir Issah <danladi496@gmail.com>
2023-02-23 12:19:55 +0000

    installed-react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import Calculator from './components/calculator';
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <Calculator />
+    <>
+    <div>
+      <nav>
+
+      </nav>
     </div>
+    </>
   );
 }
 
